2ae686b2866756e3f98834adc75f7bb6a1cd5ac0
KoSeungyu <koeul123@gmail.com>
2022-08-07 20:36:46 +0900

    스프링부트와 AWS로 혼자 구현하는 웹 서비스 chapter1

---
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freelecspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringboot2WebServiceApplication.class, args);
+    }
+
+}
