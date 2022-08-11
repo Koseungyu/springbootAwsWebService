c8fd3e8f3956313f494515567072f2bdbac21bcb
KoSeungyu <koeul123@gmail.com>
2022-08-11 23:49:18 +0900

    스프링부트와 AWS로 혼자 구현하는 웹 서비스 chapter4

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package com.jojoldu.book.freelecspringboot2webservice.service.posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostUpdateRequestDto;
+import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsListResponseDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)         //.map(posts -> new PostsListResponseDto(posts)) 와 같은 의미
+                .collect(Collectors.toList());          //.collect 이게 뭐고 어떻게 쓰는건지 살펴봐야함.
     }
 }
