102cb8699cb56e48910b03d622861b4b78228638
KoSeungyu <koeul123@gmail.com>
2022-08-12 01:18:58 +0900

    스프링부트와 AWS로 혼자 구현하는 웹 서비스 chapter4

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/posts/PostsService.java
@@ -44,7 +44,15 @@ public class PostsService {
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream()
-                .map(PostsListResponseDto::new)         //.map(posts -> new PostsListResponseDto(posts)) 와 같은 의미
-                .collect(Collectors.toList());          //.collect 이게 뭐고 어떻게 쓰는건지 살펴봐야함.
+                .map(PostsListResponseDto::new)         //.map(posts -> new PostsListResponseDto(posts)) 와 같은 의미. posts의 스트림을 map을 통해 PostsListResponseDto로 변환 List로 반환하는 메소드.
+                .collect(Collectors.toList());          //.collect 이게 뭐고 어떻게 쓰는건지 살펴봐야함. -> 요소들의 가공이 끝났다면 리턴해줄 결과를 collect를 통해 만들어준다.
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        postsRepository.delete(posts);
     }
 }
