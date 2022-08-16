1abc22f64195a35d21fec88115e9838f98fcef4f
KoSeungyu <koeul123@gmail.com>
2022-08-17 01:45:49 +0900

    스프링부트와 AWS로 혼자 구현하는 웹 서비스 chapter5

--- src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.jojoldu.book.freelecspringboot2webservice.web;
 
+import com.jojoldu.book.freelecspringboot2webservice.config.auth.SecurityConfig;
 import lombok.With;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @ExtendWith(SpringExtension.class)                        //스프링부트와 junit 사이의 연결자 역할(SpringExtension이라는 실행자를 사용)
-@WebMvcTest(controllers = HelloController.class)    //스프링어노테이션 중 WEB(Spring MVC)에만 집중할 수 있는 어노테이션(@Controller, @ControllerAdvice 사용가능)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
+//스프링어노테이션 중 WEB(Spring MVC)에만 집중할 수 있는 어노테이션(@Controller, @ControllerAdvice 사용가능)
 public class HelloControllerTest  {
 
     @Autowired
     private MockMvc mvc;                            //웹 API를 테스트 할때 사용. 스프링 MVC의 시작점. 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 가능
 
+    @WithMockUser(roles = "USER")
     @Test
     public void Hello가_리턴된다() throws Exception{
         String hello = "hello";
@@ -29,6 +38,7 @@ public class HelloControllerTest  {
                 .andExpect(content().string(hello)); //mvc.perform의 결과를 검증한다. controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증한다.
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
